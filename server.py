2b386ef87d6c681d70240903c2820e25d7e6f913
Austin Kellar <akellar@ucsc.edu>
2019-04-08 12:59:07 -0700

    broke code for exercise

--- server.py
+++ server.py
@@ -5,13 +5,4 @@ from bottle import get, route, run, template, static_file
 def index():
     return template('news.html')
 
-# ------- static files --------
-@route('/css/<filename>')
-def send_css_file(filename):
-    return static_file(filename, root='css')
-
-@route('/images/<filename>')
-def send_image(filename):
-    return static_file(filename, root='images')
-
 run(host='localhost', port=8080, debug=True)
